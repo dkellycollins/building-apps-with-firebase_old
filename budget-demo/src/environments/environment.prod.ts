3876040ab6a1862447206df8eeedc1c416457ddb
Devin Kelly-Collins <dkellycollins@gmail.com>
2020-02-09 17:44:53 -0600

    Implemented authentication

--- budget-demo/src/environments/environment.prod.ts
+++ budget-demo/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXF-5VH4TXkX6DiV5vl7V61fofWEvcRj8",
+    authDomain: "budget-demo-4e826.firebaseapp.com",
+    databaseURL: "https://budget-demo-4e826.firebaseio.com",
+    projectId: "budget-demo-4e826",
+    storageBucket: "budget-demo-4e826.appspot.com",
+    messagingSenderId: "492926800489",
+    appId: "1:492926800489:web:1693154549c3fe6181c8f8",
+    measurementId: "G-ED60BHJ022"
+  }
 };
--- budget-demo/src/environments/environment.ts
+++ budget-demo/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCXF-5VH4TXkX6DiV5vl7V61fofWEvcRj8",
+    authDomain: "budget-demo-4e826.firebaseapp.com",
+    databaseURL: "https://budget-demo-4e826.firebaseio.com",
+    projectId: "budget-demo-4e826",
+    storageBucket: "budget-demo-4e826.appspot.com",
+    messagingSenderId: "492926800489",
+    appId: "1:492926800489:web:1693154549c3fe6181c8f8",
+    measurementId: "G-ED60BHJ022"
+  }
 };
 
 /*
